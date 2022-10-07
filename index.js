4572c736dc38e1daa2ee319bc3607cf223082ca5
Rishabh044 <thefast490@gmail.com>
2022-10-08 02:25:18 +0530

    Added the functionality for box closing.

--- index.js
+++ index.js
@@ -190,6 +190,38 @@ $(".btn-close").on("click", function () {
     $(".container-rules-overlay").toggleClass("box-show");
 });
 
+const openModalBtn = document.querySelector(".btn-rules");
+const closeModalBtn = document.querySelector(".btn-close");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+console.log(modal);
+console.log(overlay);
+// close modal function
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+// close the modal when the close button and overlay is clicked
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
+
+// open modal function
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+// open modal event
+openModalBtn.addEventListener("click", openModal);
+
 /*Theme change*/
 
 const body = document.querySelector("body");
